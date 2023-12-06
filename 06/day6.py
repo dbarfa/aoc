469f7eb95db273f786ba89804d7af442f3753c32
dbarfa <contact@dbarfa.com>
2023-12-06 20:01:26 +0100

    add day 6

---
+++ 06/day6.py
@@ -0,0 +1,25 @@
+# bad code alert, speedrunned in 9 minutes
+# part 2 is bruteforced, concat the string and use same algo
+with open("input","r") as input_file:
+    lines = input_file.readlines()
+
+def solution(lines,part):
+    time = lines[0].strip().split(':')[1].strip().split()
+    distance = lines[1].strip().split(':')[1].strip().split()
+
+    if part == 2:
+        time = [''.join(time)]
+        distance = [''.join(distance)]
+
+    # let's go
+    r = 1
+    for i in range(len(time)):
+        rr = 0
+        for j in range(int(time[i])+1):
+            jj = j*(int(time[i])-j)
+            if jj > int(distance[i]):
+                rr += 1
+        r *=rr
+    return r
+
+print(solution(lines,2))
